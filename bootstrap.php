118764138c405b65933195ba5fd669c41de66d7a
Joel Hansson <joel.hansson@gmail.com>
2011-04-12 20:47:53 +0200

    initial commit

---
+++ bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+define('PROJECT_PATH', dirname(__FILE__));
+require PROJECT_PATH . '/coltrane/coltrane.php';
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require dirname(__FILE__) . '/bootstrap.php';
+function on_get()
+{
+	return "testing orchestra with coltrane";
+}
+run('index.php');
